d009fcf42b6ea6bdeff997977c2afd8bb49159a7
Ben James <bj420@ic.ac.uk>
2024-02-03 19:34:06 +0000

    export ensurePermission()

--- frontend/handemonium/src/utils/GetCameraPermission.ts
+++ frontend/handemonium/src/utils/GetCameraPermission.ts
@@ -1,10 +1,10 @@
-async function ensurePermission(): Promise<MediaStream> {
+// Test that browser allows camera permission
+export async function ensurePermission(): Promise<MediaStream> {
     try {
         const mediaStream = await navigator.mediaDevices.getUserMedia({ video: true });
 
         // Check if permission was granted
         if (mediaStream) {
-            // Your existing code for setting up the videoElement and capturing the image
             return mediaStream;
         } else {
             // Handle permission denied
