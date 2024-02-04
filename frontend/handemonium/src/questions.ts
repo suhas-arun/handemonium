a4beb514632df170ab63d517edc19f730168f501
sa2021 <sa2021@ic.ac.uk>
2024-02-04 10:19:27 +0000

    add quiz questions

--- frontend/handemonium/src/questions.ts
+++ frontend/handemonium/src/questions.ts
@@ -22,6 +22,33 @@ export const questions: QuestionType[] = [
       { text: "Optical Sensor", isCorrect: false },
     ],
   },
+  {
+    question: "What language does Node.js use?",
+    options: [
+      { text: "Java", isCorrect: false },
+      { text: "C++", isCorrect: false },
+      { text: "Python", isCorrect: false },
+      { text: "JavaScript", isCorrect: true },
+    ],
+  },
+  {
+    question: "What is the full form of HTML?",
+    options: [
+      { text: "Hyperlink and Text Markup Language", isCorrect: false },
+      { text: "Hyper Text Markup Language", isCorrect: true },
+      { text: "Home Tool Markup Language", isCorrect: false },
+      { text: "Hyper Tool Markup Language", isCorrect: false },
+    ]
+  }
+  {
+    question: "What does RISC stand for?",
+    options: [
+      { text: "Reduced Instruction Set Code", isCorrect: false },
+      { text: "Reduced Instruction Set Compiler", isCorrect: false },
+      { text: "Reduced Instruction Set Control", isCorrect: false },
+      { text: "Reduced Instruction Set Computer", isCorrect: true },
+    ]
+  }
 ];
 
 export function getCorrectAnswer(
